ef4c1e8315193df45325ea619a0f12e11b54fa58
EdgarElihuGR <edgar.elihu.garcia.ramirez@gmail.com>
2023-01-03 15:49:37 -0600

    -Added createMap and drawStatesOnMap function blueprint

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// Creating map options
-var mapOptions = {
-    center: [17.385044, 78.486671],
-    zoom: 10
-}
-    
-let map = new L.map('map' , mapOptions);
+const states = ['Jalisco', 'Morelia', 'Colima', 'Monterrey', 'Sinaloa'];
+let map;
 
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-map.addLayer(layer);
+const createMap = () => {
+    // Creating map options
+    var mapOptions = {
+        center: [23.604, -102.042],
+        zoom: 5
+    }
+        
+    map = new L.map('map' , mapOptions);
 
-let marker = new L.Marker([51.958, 9.141]);
-marker.addTo(map);
+    let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map.addLayer(layer);
+
+    let marker = new L.Marker([51.958, 9.141]);
+    marker.addTo(map);
+};
+
+const drawStatesOnMap = (states, country) => {
+    for(let state of states){
+        let api = `https://nominatim.openstreetmap.org/search/?state=${state}&country=${country}&polygon_svg=1&format=json`;
+        fetch(api)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+ }
+
+createMap();
+drawStatesOnMap(states, 'Mexico')
